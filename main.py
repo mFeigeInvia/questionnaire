2b6c1f06d16f6fd78dfc4c64ba25fe04e41a61e2
mFeigeInvia <97728206+mFeigeInvia@users.noreply.github.com>
2024-11-12 10:56:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -36,3 +36,7 @@ def process():
 
 
 # make 100 GET request on google.com and print the http status code, on a http error stop the execusion and print the status test
+#...
+
+
+# create a postgres connection with psycopg3 and query a table test like you would do in production environment
