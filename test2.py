5aaef7429baf94e56490fd0779a0bc25477f338d
mFeigeInvia <97728206+mFeigeInvia@users.noreply.github.com>
2024-11-13 09:45:46 +0100

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,8 @@
+# iterate over Sheet and combine booth strings with a "-" in the middle
+class Sheet:
+    def __init__(self, data: list[str, str]):
+        self.__data: list = data
+
+
+def process():
+    pass
