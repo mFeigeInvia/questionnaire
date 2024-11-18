53dffce17b6502cd21c912f1a10fc07bb1902f74
mFeigeInvia <97728206+mFeigeInvia@users.noreply.github.com>
2024-11-18 11:45:42 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,6 +1,6 @@
 # iterate over Sheet and combine booth strings with a "-" in the middle
 class Sheet:
-    def __init__(self, data: list[str, str]):
+    def __init__(self, data: list[dict(str, str)]):
         self.__data: list = data
 
 
